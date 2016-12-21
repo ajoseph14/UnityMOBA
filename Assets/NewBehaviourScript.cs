dac3c0e4cf5856b855ab00b7245735455431f0df
DESKTOP-AJOSEPH\ajose <ajoseph.014@gmail.com>
2016-12-21 21:23:29 +0400

    First commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	float speed =10.0F;
+	float rotationSpeed =100.0F;
+	
+	// Update is called once per frame
+	void Update () {
+		float translation = CrossPlatformInputManager.GetAxis ("Vertical") * speed;
+		float rotation = CrossPlatformInputManager.GetAxis ("Horizontal") * rotationSpeed;
+
+		translation *= Time.deltaTime;
+		rotation *= Time.deltaTime;
+		transform.Translate (0, 0, translation);
+		transform.Rotate (0, rotation, 0);
+	}
+}
---
+++ Assets/SetupLocalPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SetupLocalPlayer : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (isLocalPlayer)
+            GetComponent<NewBehaviourScript>().enabled = true;
+        SmoothCameraFollow.target = this.transform;
+	}
+
+}
